6f91ce1f468be2799fd275f8f06bef51929b0d1a
Advaita Krishna das <akd@akd.local>
2019-11-09 12:33:04 +0600

    don't override params

--- sarasvati/packages/metadata/yaml_parser.py
+++ sarasvati/packages/metadata/yaml_parser.py
@@ -12,16 +12,16 @@ class YamlMetadataParser(IMetadataParser):
     def parse(self, data: str) -> Dict[str, Package]:
         # parse yaml file
         try:
-            data = yaml_load(data)
+            meta_data = yaml_load(data)
         except ScannerError as ex:
             raise PackagesException(f"Invalid YAML file: {ex.problem}")
 
         # string is a valid yaml, but we are expecting list of packages
-        if not isinstance(data, dict):
+        if not isinstance(meta_data, dict):
             raise PackagesException("Metadata is not a dictionary")
 
         # convert dictionary into Package objects
         return {
             k: Package(k, v["name"], v["description"], v["url"], v["author"])
-            for k, v in data.items()
+            for k, v in meta_data.items()
         }
