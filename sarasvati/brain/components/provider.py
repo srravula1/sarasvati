b57b79cc9314f51772b4b9a69d1c148a497d1cc9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-11-09 16:33:21 +0600

    make type hinting correct

--- sarasvati/brain/components/provider.py
+++ sarasvati/brain/components/provider.py
@@ -1,8 +1,10 @@
-from typing import List
+from abc import ABCMeta, abstractmethod
+from typing import Dict
 
 from sarasvati.brain.components.info import ComponentInfo
 
 
-class ComponentsInfoProvider:
-    def load_components(self) -> List[ComponentInfo]:
+class ComponentsInfoProvider(metaclass=ABCMeta):
+    @abstractmethod
+    def load_components(self) -> Dict[str, ComponentInfo]:
         pass
